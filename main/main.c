d76fe953853f88be4c558ae201a179a3df64e8e2
Marcone Tenorio <marconetsf@gmail.com>
2022-09-15 01:30:16 +0000

    adding nvsflash access

--- main/main.c
+++ main/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "nvs_flash.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
+    esp_err_t ret;
 
+    // Initialize NVS.
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
 }
