9836cb67933b1f053e83114b940d01efedf64da3
Marcone Tenorio <marconetsf@gmail.com>
2022-09-15 12:35:37 +0000

    adding main loop task

--- main/main.c
+++ main/main.c
@@ -5,15 +5,40 @@
 #include "nvs_flash.h"
 #include "esp_log.h"
 
-void app_main(void)
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+const char TAG[] = "TRACTIAN_CHALLENGE";
+
+void setup(void)
 {
     esp_err_t ret;
 
-    // Initialize NVS.
+    /*Será necessário iniciar o acesso a flash para acessar
+    o arquivo de 500kb pois a memória ram das esp32 possui apenas 512kb */
+
     ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+
+    ESP_LOGI(TAG, "NVS inicializado com sucesso");
+}
+
+void loop_routine(void* pvParameter)
+{
+    while(1)
+    {
+        vTaskDelay(100/portTICK_PERIOD_MS); //delay para evitar watchdog
+    }
+}
+
+void app_main(void)
+{
+    setup();
+    xTaskCreate(loop_routine, "BLUETOOTH_ROUTINE", 60*1024, NULL, 3, NULL);
 }
